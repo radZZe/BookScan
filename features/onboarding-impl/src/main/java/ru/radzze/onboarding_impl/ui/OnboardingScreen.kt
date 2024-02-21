65b93dbb21ce91b3109914e15a59db287c61ae33
radZZe <mimozadr@gmail.com>
2024-02-21 12:43:17 +1000

    finish onboarding module

--- features/onboarding-impl/src/main/java/ru/radzze/onboarding_impl/ui/OnboardingScreen.kt
+++ features/onboarding-impl/src/main/java/ru/radzze/onboarding_impl/ui/OnboardingScreen.kt
@@ -4,16 +4,27 @@ package ru.radzze.onboarding_impl.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -41,11 +52,13 @@ fun OnboardingScreen(
     val coroutineScope = rememberCoroutineScope()
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         HorizontalPager(
-            modifier = Modifier.fillMaxWidth(0.95f).fillMaxSize(0.8f),
+            modifier = Modifier
+                .fillMaxHeight(0.8f)
+                .fillMaxWidth(),
             count = 5,
             state = pagerState
         ) { pos ->
@@ -59,9 +72,14 @@ fun OnboardingScreen(
             modifier = Modifier,
             text = pages[pagerState.currentPage].btnText,
             onClick = {
-                coroutineScope.launch {
-                    pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                if (pagerState.currentPage == 4) {
+                    onNavigateToLogin()
+                } else {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                    }
                 }
+
             })
 
     }
@@ -73,17 +91,32 @@ fun OnboardingContent(
     modifier: Modifier = Modifier,
     item: OnboardingPage
 ) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Image(
-            modifier = Modifier.fillMaxWidth(),
-            painter = painterResource(id = item.image),
-            contentDescription = null
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize(0.8f),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Image(
+                modifier = Modifier
+                    .size((constraints.maxHeight - 50).dp)
+                    .align(Alignment.TopCenter),
+                painter = painterResource(id = item.image),
+                contentDescription = null
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(0.85f),
+            text = item.description,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp,
+            color = Color(0,0,0)
         )
-        Text(text = item.description)
     }
 }
 
@@ -94,7 +127,8 @@ fun OnboardingButton(
     onClick: () -> Unit
 ) {
     Button(modifier = modifier, onClick = { onClick() }) {
-        Text(text = text)
+        Text(text = text,color = Color(0,0,0))
     }
 
 }
+
